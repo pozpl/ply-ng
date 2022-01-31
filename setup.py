53bf9613458caae666703563f76581b3e7bf2b8e
Pavel Pozdnyak <pozdpl@gmail.com>
2022-01-31 23:05:11 +1300

    updating setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='ply-ng',
       version='0.1',
       description='learning python and pandas emulating dplyr',
-      url='https://willcomelater.org',
+      url='https://github.com/pozpl/ply-ng',
       author='Pavel Pozdniak',
       author_email='pozdpl@gmail.com',
       license='MIT',
